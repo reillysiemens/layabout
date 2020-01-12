8562f859585f44235e900456d0fabb52a941ed64
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2020-01-11 16:39:06 -0800

    Add Python 3.8 support for a final release

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         'Operating System :: POSIX :: BSD :: FreeBSD',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Communications :: Chat',
